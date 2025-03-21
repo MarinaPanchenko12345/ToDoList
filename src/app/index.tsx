1015a9c73bdb703e63c97347ca5456eeac3e048d
Marina Panchenko <marishka100281@gmail.com>
2025-03-21 07:38:39 +0200

    first commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -38,7 +38,7 @@ function App() {
         <div className={styles.todoText}>
           <p>There is no tasks</p>
           <img
-            src='/pablic/page-concept.png'
+            src='/public/page-concept.png'
             alt='No tasks'
             className={styles.emptyImage}
           />
